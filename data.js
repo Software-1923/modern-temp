4922d2482c74c239a481f5210cdb38155d89a7a7
Software-1923 <143068984+Software-1923@users.noreply.github.com>
2023-11-07 20:01:01 +0300

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'));
